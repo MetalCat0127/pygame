9aad0d545445f643ee5682e7567a9d75c235e5d0
MetalCat0127 <syou1205.hasegawa@gmail.com>
2026-06-15 10:37:21 +0900

    サイコロの設定

--- start_game.py
+++ start_game.py
@@ -1,5 +1,15 @@
 import json
 import js
 
+def __init__():
+    dice_faces = [
+        [1,2,3,4,5,6],
+        [1,2,3,4,5,6],
+        [1,2,3,4,5,6],
+        [1,2,3,4,5,6],
+        [1,2,3,4,5,6]
+    ]
+    js.setDiceFaces(dice_faces) #ここでjsに初期値を渡す
+
 def on_enemy_defeated():
     js.addExp(20)  # JSの経験値バーを更新
