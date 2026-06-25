f480c90eba9c32590d4706b4e8dca4c1bfdc31c1
MetalCat0127 <syou1205.hasegawa@gmail.com>
2026-06-25 12:36:49 +0900

    うーん

--- start_game.py
+++ start_game.py
@@ -1,7 +1,7 @@
 import js
 
 def init_game(stage_name):
-    stage = js.stage_data.to_py()  # ← JS から渡された JSON を受け取る
+    stage = stage_data  # ← JS から渡された JSON を受け取る
 
     dice_faces = stage["dice"]
     enemy_list = stage["enemies"]
