ec146368244fd31c87c3f9f15459bc5f4292f2f5
MetalCat0127 <syou1205.hasegawa@gmail.com>
2026-08-02 19:08:55 +0900

    これだな？

--- start_game.py
+++ start_game.py
@@ -41,6 +41,7 @@ def init_game(stage_name):
 
     print("ゲーム初期化完了:", stage_name)
 
+    globals()["start_wave"] = fight.start_wave
     globals()["on_attack_button"] = fight.on_attack_button
     globals()["apply_skill"] = fight.apply_skill
     globals()["get_skill_choices"] = skill.get_skill_choices
